d1c12cec70d7f972e29bf0edc7883024c71baa6c
Andrew <andrewmocol@hotmail.com>
2022-05-28 21:30:38 -0500

    fix routing and home

---
+++ website/.docusaurus/routes.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '31a'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '8da'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'ab9'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'ffa'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', 'ef2'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '783'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '468'),
+    exact: true
+  },
+  {
+    path: '/blog',
+    component: ComponentCreator('/blog', '772'),
+    exact: true
+  },
+  {
+    path: '/blog/2016/03/11/blog-post',
+    component: ComponentCreator('/blog/2016/03/11/blog-post', '35f'),
+    exact: true
+  },
+  {
+    path: '/blog/2017/04/10/blog-post-two',
+    component: ComponentCreator('/blog/2017/04/10/blog-post-two', 'bf2'),
+    exact: true
+  },
+  {
+    path: '/blog/2017/09/25/testing-rss',
+    component: ComponentCreator('/blog/2017/09/25/testing-rss', '466'),
+    exact: true
+  },
+  {
+    path: '/blog/2017/09/26/adding-rss',
+    component: ComponentCreator('/blog/2017/09/26/adding-rss', '721'),
+    exact: true
+  },
+  {
+    path: '/blog/2017/10/24/new-version-1.0.0',
+    component: ComponentCreator('/blog/2017/10/24/new-version-1.0.0', '57b'),
+    exact: true
+  },
+  {
+    path: '/blog/archive',
+    component: ComponentCreator('/blog/archive', 'f18'),
+    exact: true
+  },
+  {
+    path: '/docs',
+    component: ComponentCreator('/docs', '329'),
+    routes: [
+      {
+        path: '/docs/doc1',
+        component: ComponentCreator('/docs/doc1', '7ad'),
+        exact: true,
+        sidebar: "defaultSidebar"
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', 'c88'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
---
+++ website/docusaurus.config.js
@@ -0,0 +1,103 @@
+module.exports={
+  title: 'Kin SDK Dart',
+  tagline: 'Easily implement Kin and earn',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  organizationName: 'Kinny',
+  projectName: 'Kin SDK',
+  scripts: [
+    'https://buttons.github.io/buttons.js'
+  ],
+  // deploymentBranch: 'gh-pages',
+  favicon: 'img/favicon.ico',
+  onBrokenLinks: 'log',
+  onBrokenMarkdownLinks: 'log',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // sidebarCollapsible: true,
+          // editUrl: 'https://github.com/kinnytips/kin-sdk-documentation/edit/main/website',
+          // showLastUpdateAuthor: true,
+          // showLastUpdateTime: true,
+          // path: '..\\docs',
+          // sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: {
+          path: 'blog'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/customTheme.css'),
+        }
+      }
+    ]
+  ],
+  plugins: [],
+  themeConfig: {
+    forceDarkMode: true, 
+    darkMode: true,
+    // docs: {
+    //   sidebar: {
+    //     hideable: true,
+    //     autoCollapseCategories: true,
+    //   },
+    // },
+    colorMode: {
+      // "light" | "dark"
+      defaultMode: 'dark',
+
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: true,
+
+      // Should we use the prefers-color-scheme media-query,
+      // using user system preferences, instead of the hardcoded defaultMode
+      respectPrefersColorScheme: false,
+
+      // Dark/light switch icon options
+      // switchConfig: {
+      //   // Icon for the switch while in dark mode
+      //   darkIcon: '  ',
+      //   darkIconStyle: {
+      //     marginTop: '1px',
+      //   },
+      //   lightIcon: '  ',
+      //   lightIconStyle: {
+      //     marginTop: '1px',
+      //   },
+      // },
+    },
+    navbar: {
+      title: 'Test Site',
+      // logo: {
+      //   src: 'img/favicon.ico'
+      // },
+      items: [
+        {
+          to: '/docs/',
+          label: 'Docs',
+          position: 'left'
+        },
+        {
+          to: '/blog/',
+          label: 'Blog',
+          position: 'left'
+        },
+        {
+          to: '/help',
+          label: 'Help',
+          position: 'left'
+        }
+      ]
+    },
+    image: 'img/undraw_online.svg',
+    footer: {
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Kinny Technologies Inc.`,
+      logo: {
+        src: 'img/favicon.ico'
+      }
+    }
+  }
+}
